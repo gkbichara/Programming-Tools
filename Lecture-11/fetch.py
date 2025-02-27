24a507cb3caaecfc1c07e0a093983e0d1f45825f
Galal Bichara <galal.bichara@gmail.com>
2025-02-27 15:57:32 -0500

    Finished fetch file

--- Lecture-11/fetch.py
+++ Lecture-11/fetch.py
@@ -1,14 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from datetime import datetime
 
 url = 'https://www.bbc.com/'
 
 response = requests.get(url)
 data = response.text
 
-# features = "html.parser" written to avoid warning message
-soup = BeautifulSoup(data, features = "html.parser")
+soup = BeautifulSoup(data)
 text = soup.find_all('h2')
 
-news = [t.text for t in soup.find_all('h2')]
+news = [t.text for t in soup.find_all('h2', {'class': 'sc-87075214-3 eywmDE'})]
+
+f = open(str(datetime.now()) + '.txt', 'w')
+f.write('\n'.join(news))
+f.close()
